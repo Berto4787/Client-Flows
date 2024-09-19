eed813b70670a85b961ee30636060890fe4cd64a
Berto4787 <62498320+Berto4787@users.noreply.github.com>
2024-09-19 09:15:35 +0300

    Update Client_Flows.py

--- Client_Flows.py
+++ Client_Flows.py
@@ -130,6 +130,7 @@ if st.session_state['calc_type'] == 'EoD':
                                                                                                            np.subtract(st.session_state['prev_day_pos_calc']['THEORETICAL PRICE'],st.session_state['prev_day_pos_calc']['EOD PRICE T-1']))),
                                                                                            'PENDING PREMIUM': 0.})
 elif st.session_state['calc_type'] == 'ItD':
+    st.dataframe(st.session_state['prev_day_pos_calc'])
     st.session_state['prev_day_pos_calc'] =st.session_state['prev_day_pos_calc'].assign(**{'RVM': np.where(st.session_state['prev_day_pos_calc'].SYMBOL!='Future', 0.,
                                                                                                            np.multiply(np.multiply(st.session_state['prev_day_pos_calc'].QUANTITY,
                                                                                                                                    st.session_state['prev_day_pos_calc']['CONTRACT SIZE']),
