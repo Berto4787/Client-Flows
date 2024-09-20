ac984a33e092ac3ebdef7e47052c8854563951af
Berto4787 <62498320+Berto4787@users.noreply.github.com>
2024-09-20 17:26:02 +0200

    Update Client_Flows.py

--- Client_Flows.py
+++ Client_Flows.py
@@ -10,12 +10,12 @@ st.set_page_config(
 st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>IM_SINGLE_POSITION - QCCP</p>", unsafe_allow_html=True)
 qccp_margins = pd.DataFrame({'SYMBOL':['Future', 'Call', 'Put'], 'LONG':[1000, 180, 130], 'SHORT':[950, 190, 145]})
 qccp_margins = qccp_margins.set_index('SYMBOL')
-st.session_state['qccp_margins'] = st.sidebar.data_editor(qccp_margins, disabled=('SYMBOL'))
+st.session_state['qccp_margins'] = st.sidebar.data_editor(qccp_margins, disabled=('SYMBOL'), use_container_width=True)
 st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>ITD THEORETICAL PRICES - MITCH</p>", unsafe_allow_html=True)
 theor_prices = pd.DataFrame({'SYMBOL':['Future', 'Call', 'Put'], 'THEORETICAL PRICE':[9500., 5.3, 3.2],
                             'CONTRACT SIZE':[1000, 5, 5]})
 theor_prices = theor_prices.set_index('SYMBOL')
-st.session_state['theor_prices'] = st.sidebar.data_editor(theor_prices, disabled=('SYMBOL', 'CONTRACT SIZE'), use_container_width=True)
+st.session_state['theor_prices'] = st.sidebar.data_editor(theor_prices, disabled=('SYMBOL', 'CONTRACT SIZE'))
 st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>EOD PRICES- MITCH OR QCCP REPORTS</p>", unsafe_allow_html=True)
 eod_prices = pd.DataFrame({'SYMBOL':['Future', 'Call', 'Put'], 'EOD PRICE T':[9650., 5.5, 3.1],
                           'EOD PRICE T-1':[9300., 4.1, 2.5]})
