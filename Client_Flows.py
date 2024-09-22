ac43d064f38f89daa57cfede00476f8efcdd7144
Berto4787 <62498320+Berto4787@users.noreply.github.com>
2024-09-22 08:10:35 +0300

    Update Client_Flows.py

--- Client_Flows.py
+++ Client_Flows.py
@@ -76,7 +76,7 @@ with st.container():
     st.session_state['new_side']  = st.selectbox('Side', options=['Buy', 'Sell'], index=0)
     st.session_state['new_type']  = st.selectbox('Type', options=['Order', 'Trade'], index=0)
     st.text_area("",
-                 """ Use 'Order' to submit outstanding orders and 'Trade' for executed Trades""",
+                 """ Use 'Order' to submit outstanding orders (either placed on T or carried forward orders from T-1) and 'Trade' for executed trades.""",
                  disabled=True)
     submit_button = st.button(label='Submit')
 
@@ -185,7 +185,7 @@ with st.expander('Click to see break down'):
             st.text_area("",
                          """ Realized Variation Margin (RVM) should be computed EoD by B/O system to MtM each trade on futures to EoD official settlement price.
                          RVM = Quantity * Contract Size * (EoD Price T - Execution Price)
-                         Pending Premium should be computed both ItD & EoD by B/O system for trades in options, to compute the premium settlement as part of the clearing obligations:
+                    Pending Premium should be computed both ItD & EoD by B/O system for trades in options, to compute the premium settlement as part of the clearing obligations:
                          Pending Premium = Quantity * Contract Size * Execution Premium""", disabled=True)
             
         elif st.session_state['calc_type'] == 'ItD':
@@ -194,7 +194,7 @@ with st.expander('Click to see break down'):
             st.text_area("",
                          """ Contingent Variation Margin (CVM) should be computed ItD by F/O system for each trade on futures as a component of Buying Power computation.
                          CVM = Quantity * Contract Size * (ItD Theoretical Price - Execution Price)
-                         Pending Premium should be computed both ItD & EoD by B/O system for trades in options, to compute the premium settlement as part of the clearing obligations:
+                    Pending Premium should be computed both ItD & EoD by B/O system for trades in options, to compute the premium settlement as part of the clearing obligations:
                          Pending Premium = Quantity * Contract Size * Execution Premium""", 
                          disabled=True)
        
