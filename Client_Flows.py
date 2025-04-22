0a41cdf44762fb7da094d9b8e8d2bfba56d19790
Berto4787 <62498320+Berto4787@users.noreply.github.com>
2025-04-22 08:10:36 +0300

    Update Client_Flows.py

--- Client_Flows.py
+++ Client_Flows.py
@@ -48,6 +48,7 @@ sod_collateral = pd.DataFrame({'CLIENT': ['Client 1', 'Client 2', 'Client 3'],
                             index= np.arange(3))
 sod_collateral = sod_collateral.set_index('CLIENT')
 st.session_state['sod_collateral'] = st.sidebar.data_editor(sod_collateral, disabled=('CLIENT'), use_container_width=True)
+st.session_state['client_bp'] = st.session_state['sod_collateral']
 
 st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>CM COLLATERAL AT CCP</p>", unsafe_allow_html=True)
 sod_collateral_ccp = pd.DataFrame({'COLLATERAL ACCOUNT': ['OSA'],
@@ -385,7 +386,6 @@ if st.session_state['open_pos'].shape[0]>0:
         with st.container():
             cli, ccp = st.columns([1,1])
             # Client - Broker
-            st.session_state['client_bp'] = st.session_state['sod_collateral']
             if st.session_state['open_pos'].shape[0]:
                 open_pos_req = st.session_state['open_pos'].pivot_table(index=['CLIENT'], values=['TOTAL REQUIREMENT'],
                                                                         aggfunc='sum')
