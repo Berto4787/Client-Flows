59f6cf97941e8de728ef4e7a48f9c9063f808047
Berto4787 <62498320+Berto4787@users.noreply.github.com>
2025-04-27 14:27:46 +0300

    Update Client_Flows.py

--- Client_Flows.py
+++ Client_Flows.py
@@ -95,13 +95,13 @@ with st.container():
 
     if st.session_state['new_type'] == 'Order':
         new_pos = new_pos.assign(**{'INITIAL MARGIN': np.where((st.session_state['new_instrument']!='Future') & (st.session_state['new_side']=='Buy'), 0.,
-                                                               st.session_state['new_quantity'] * st.session_state['fit_margins'].loc[st.session_state['new_instrument']]['IM'])})
+                                                               -1*st.session_state['new_quantity'] * st.session_state['fit_margins'].loc[st.session_state['new_instrument']]['IM'])})
         new_pos = new_pos.assign(**{'PENDING PREMIUM': np.where(st.session_state['new_instrument']=='Future', 0.,
                                                                 np.where(st.session_state['new_side'] == 'Buy', -1, 1) * st.session_state['new_quantity'] * st.session_state['new_price'] * st.session_state['theor_prices'].loc[st.session_state['new_instrument']]['CONTRACT SIZE'])})
         new_pos = new_pos.assign(**{'NLV': np.where(st.session_state['new_instrument']=='Future', 0.,
                                                                 np.where(st.session_state['new_side'] == 'Buy', 0., -1) * st.session_state['new_quantity'] * st.session_state['theor_prices'].loc[st.session_state['new_instrument']]['THEORETICAL PRICE'] * st.session_state['theor_prices'].loc[st.session_state['new_instrument']]['CONTRACT SIZE'])})
         new_pos = new_pos.assign(**{'CURRENT BUYING POWER':st.session_state['client_bp'].loc[st.session_state['new_client']]['BUYING POWER'],
-                                    'TOTAL REQUIREMENT': np.absolute(np.minimum(np.add(np.add(np.multiply(-1, new_pos['INITIAL MARGIN']), new_pos['PENDING PREMIUM']), new_pos['NLV']), 0))})
+                                    'TOTAL REQUIREMENT': np.absolute(np.minimum(np.add(np.add(new_pos['INITIAL MARGIN'], new_pos['PENDING PREMIUM']), new_pos['NLV']), 0))})
         new_pos = new_pos.assign(**{'STATUS': np.where(new_pos['CURRENT BUYING POWER'] > new_pos['TOTAL REQUIREMENT'], 'ACCEPTED', 'REJECTED')})
     
         if 'orders' not in st.session_state.keys():
