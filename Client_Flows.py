c16c1939965d14ce3573f504f6c2c736c079083a
Berto4787 <62498320+Berto4787@users.noreply.github.com>
2025-04-27 14:49:35 +0300

    Update Client_Flows.py

--- Client_Flows.py
+++ Client_Flows.py
@@ -246,7 +246,7 @@ if 'open_pos' in st.session_state.keys():
     st.session_state['open_pos'] = st.session_state['open_pos'].assign(**{'MAINTENANCE MARGIN': -1 * np.abs(np.where((st.session_state['open_pos']['SYMBOL']!='Future') & (st.session_state['open_pos']['QUANTITY']>0), 0.,
                                                                                                                  np.multiply(st.session_state['open_pos']['QUANTITY'], st.session_state['open_pos']['MM']))),
                                                                          'NLV': np.where((st.session_state['open_pos']['SYMBOL']!='Future') & (st.session_state['open_pos']['QUANTITY']<0), 
-                                                                                         -1 * st.session_state['open_pos']['QUANTITY'] * st.session_state['theor_prices'].loc[st.session_state['open_pos']['SYMBOL'].values[0]]['THEORETICAL PRICE'] * st.session_state['theor_prices'].loc[st.session_state['open_pos']['SYMBOL'].values[0]]['CONTRACT SIZE'],0)})
+                                                                                         st.session_state['open_pos']['QUANTITY'] * st.session_state['theor_prices'].loc[st.session_state['open_pos']['SYMBOL'].values[0]]['THEORETICAL PRICE'] * st.session_state['theor_prices'].loc[st.session_state['open_pos']['SYMBOL'].values[0]]['CONTRACT SIZE'],0)})
     if st.session_state['calc_type'] == 'EoD':
         st.session_state['open_pos'] = st.session_state['open_pos'].assign(**{'TOTAL REQUIREMENT': np.add(st.session_state['open_pos']['MAINTENANCE MARGIN'], st.session_state['open_pos']['NLV'])})
     elif st.session_state['calc_type'] == 'ItD':   
